f5041defa9a634518b01d2a7fa613746087ae2b5
Jordi <jordirojo@gmail.com>
2019-02-13 09:42:17 +0100

    libraries

---
+++ Inicialization and Libraries.R
@@ -0,0 +1,29 @@
+
+
+
+#strsplit(version[['version.string']], ' ')[[1]][3]
+
+
+#install.packages("Rserve")
+library(esquisse)
+
+
+
+#### Load Libraries ####
+library(pacman)
+pacman::p_load(readr, caret, plotly, ggplot2, 
+               labeling, promises, ggridges, 
+               doParallel, mlbench,# inum, e1071, 
+               corrplot#, ggpubr
+               , rpart, rpart.plot, gbm
+               , boot, dplyr,
+               reshape, Rserve#, tidyvers
+               , padr)
+
+#library(proj4)
+
+#### Enable parallel processing ####
+no_cores <- detectCores() - 1
+cl <- makeCluster(no_cores)
+registerDoParallel(cl)
+on.exit(stopCluster(cl))
