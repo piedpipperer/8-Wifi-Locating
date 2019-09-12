df1a7748a18b5d1721faea9d954aad3249836efd
Jordi <jordirojo@gmail.com>
2019-09-12 12:15:30 +0200

    gg

--- 1 - Inicialization and Libraries.R
+++ 1 - Inicialization and Libraries.R
@@ -11,12 +11,17 @@ libraries_function <- function(){
     library('pacman')
     pacman::p_load(anytime,tidyverse,lubridate,plotly,cowplot,reshape2,expss,lattice,ggplot2,rpart,esquisse,xgboost
                    ,randomForest,liquidSVM,DMwR,kknn,C50,e1071,doParallel,zoo,caret, 
-                   shiny,rsconnect,shiny,shinydashboard,flexdashboard)
+                   shiny,rsconnect,shiny,shinydashboard,flexdashboard #,rstudioapi
+                   )
   } else {
     library('pacman')
     pacman::p_load(anytime,tidyverse,lubridate,plotly,cowplot,reshape2,expss,lattice,ggplot2,rpart,xgboost
                    ,randomForest,liquidSVM,DMwR,kknn,C50,e1071,doParallel,zoo,caret, 
-                   shiny,rsconnect,shiny,shinydashboard,flexdashboard)
+                   shiny
+                   #,rsconnect,shiny,shinydashboard
+                   ,flexdashboard 
+                   #,rstudioapi
+                   )
   }
   
 }
